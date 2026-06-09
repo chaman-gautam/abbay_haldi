b13bcee339a22dec0eea1b0475a7bf0095af5d30
Chaman Gautam <cgautam4213@gmail.com>
2026-06-09 12:37:12 +0530

    the final commit for mail sending

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
       /* Email to owner */
       await transporter.sendMail({
         from: `"Booking" <${process.env.SMTP_USER}>`,
-        to:process.env.SMTP_USE,
+        to:process.env.SMTP_USER,
         replyTo: data.email,
         subject: "New Booking Received",
         html: `
